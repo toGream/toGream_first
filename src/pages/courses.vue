84db92ff9f8eadaeee17ec4e8fae9e9c4ca45b60
zhongyiliang <zhongyiliang@163.com>
2018-11-11 20:26:57 +0800

    修改组件

--- src/pages/courses.vue
+++ src/pages/courses.vue
@@ -9,6 +9,49 @@
 				</div>
 			</div>
 		</div>
+
+        <div id="inner-content">
+            <div class="container">
+                <div class="course">
+                    <div class="list-row">
+                        <div class="col-lg-12">
+                            <div>
+                                <h3>Courses We Offer</h3>
+                                <span class="clear spacer_responsive_hide_mobile ">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus temporibus perferendis nesciunt quam repellendus nulla nemo ipsum odit corrupti consequuntur possimus, vero mollitia velit ad consectetur. Alias, laborum excepturi nihil autem nemo numquam, ipsa architecto non, magni consequuntur quam.
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="list-row1" v-for="courses in courseList">
+                        <div class="col-md-4" v-for="course in courses">
+                            <div class="textbox">
+                                <h3>{{course.name}}</h3>
+                                <p>
+                                    {{course.content}}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus temporibus perferendis nesciunt quam repellendus nulla nemo ipsum odit corrupti consequuntur possimus
+                </p>
+                <br>
+
+                <div class="list-row service-v1 margin-bottom-40">
+                    <div class="col-md-4 md-margin-bottom-40" v-for="service in serviceList">
+                        <img class="img-responsive" :src="service.url">
+                        <h3>{{service.title}}</h3>
+                        <p>
+                            {{service.content}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 	</div>
 </template>
 <script>
@@ -16,7 +59,25 @@ export default{
 	name:'courses',
 	data(){
 		return {
-
+            courseList:[
+                [{name:'Heading Course',content:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vest sit amet, consec ibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.'},
+                {name:'Heading Course',content:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vest sit amet, consec ibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.'},
+                {name:'Heading Course',content:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vest sit amet, consec ibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.'}],
+                [{name:'Heading Course',content:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vest sit amet, consec ibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.'},
+                {name:'Heading Course',content:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vest sit amet, consec ibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.'},
+                {name:'Heading Course',content:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vest sit amet, consec ibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.'}]
+            ],
+            serviceList:[
+                {title:'Web Development',
+                 content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus temporibus perferendis nesciunt quam repellendus nulla nemo ipsum odit corrupti consequuntur possimus',
+                 url:require('../assets/img/service1.jpg')},
+                 {title:'Mobile Development',
+                 content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus temporibus perferendis nesciunt quam repellendus nulla nemo ipsum odit corrupti consequuntur possimus',
+                 url:require('../assets/img/service2.jpg')},
+                 {title:'Responsive Design',
+                 content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus temporibus perferendis nesciunt quam repellendus nulla nemo ipsum odit corrupti consequuntur possimus',
+                 url:require('../assets/img/service3.jpg')}
+            ]
 		}
 	}
 }	
@@ -72,5 +133,72 @@ h2 {
     -webkit-margin-after: 0.83em;
     -webkit-margin-start: 0px;
     -webkit-margin-end: 0px;
+}
+#inner-content {
+    position: relative;
+    background: #fff;
+    padding: 50px 0 0px 0;
+    width: 100%;
+}
+h1, h2, h3, h4, h5, h6 {
+    font-family: 'Open Sans', Arial, sans-serif;
+    font-weight: 700;
+    line-height: 1.1em;
+    color: #333;
+    margin-bottom: 20px;
+}
+h1, h2, h3{
+    margin-top: 20px;
+    margin-bottom: 10px;
+}
+h3{
+    font-size: 24px;
+    display: block;
+    -webkit-margin-before: 1em;
+    -webkit-margin-after: 1em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+}
+.clear{
+    clear: both;
+}
+.spacer_responsive_hide_mobile{
+    height: 13px;
+    display: block;
+}
+.list-row1{
+    margin-left: -15px;
+    margin-right: -15px;
+    display: inline-block;
+}
+.col-md-4 {
+    width: 30%;
+    float: left;
+    padding:15px;
+}
+.textbox {
+    background-color: #EFEFEF;
+    padding: 4px 25px;
+}
+p {
+    margin: 0 0 10px;
+    display: block;
+    -webkit-margin-before: 1em;
+    -webkit-margin-after: 1em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+}
+#content img {
+    max-width: 100%;
+    height: auto;
+}
+.img-responsive {
+    display: block;
+    max-width: 100%;
+    height: auto;
+}
+img {
+    vertical-align: middle;
+    border: 0;
 }
 </style>
