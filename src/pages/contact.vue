e6461a15d93a4e0223db65f801fe77625fde8800
zhongyiliang <zhongyiliang@163.com>
2018-11-11 16:40:28 +0800

    增加组件样式

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,6 +1,14 @@
 <template>
 	<div>
-		contact
+		<div id="inner-headline">
+			<div class="container">
+				<div class="list-row">
+					<div class="col-lg-12">
+						<h2 class="pageTitle">Contact</h2>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
@@ -14,5 +22,55 @@ export default{
 }	
 </script>
 <style scoped>
+#inner-headline{
+	background: #14A085;
+    position: relative;
+    margin: 0;
+    padding: 0;
+    color: #fefefe;
+    border-top: 10px solid #11967C;
+}
+.container{
+	padding: 0 20px 0 20px;
+    position: relative;
+    width: 1170px;
+    margin-right: auto;
+    margin-left: auto;
 
+}
+.list-row{
+	margin-bottom: 30px;
+	margin-left: -15px;
+    margin-right: -15px;
+    display: inline-block;
+}
+.col-lg-12 {
+    width: 100%;
+    position: relative;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+.pageTitle {
+    color: #fff;
+    margin: 30px 0 3px;
+}
+.div{
+	display: block;
+}
+h1, h2, h3, h4, h5, h6 {
+    font-family: 'Open Sans', Arial, sans-serif;
+    font-weight: 700;
+    line-height: 1.1em;
+    color: #333;
+    margin-top: 20px;
+}
+h2{
+    font-size: 30px;
+}
+h2 {
+    -webkit-margin-before: 0.83em;
+    -webkit-margin-after: 0.83em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+}
 </style>
--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -1,6 +1,38 @@
 <template>
 	<div>
-		portfolio
+		<div id="inner-headline">
+			<div class="container">
+				<div class="list-row">
+					<div class="col-lg-12">
+						<h2 class="pageTitle">Portfolio</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div id="inner-content">
+            <div class="container">
+                <div class="list-row">
+                    <div class="col-lg-12">
+
+                        <ul class="portfolio-categ filter">
+                            <li v-for="topic in topics">
+                                <a href="#">{{topic}}</a>
+                            </li>
+                        </ul>
+
+                        <div class="list-row">
+                            <div id="projects">
+                                <ul id="thumbs">
+                                    <li class="item-thumbs col-lg-3" v-for="thumb in thumbs">
+                                        <img :src="thumb.url"/>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 	</div>
 </template>
 <script>
@@ -8,11 +40,155 @@ export default{
 	name:'portfolio',
 	data(){
 		return {
-
+            topics:["All","Web design","Mobile App","UI design"],
+            thumbs:[
+                {url:require("../assets/img/works/1.jpg")},
+                {url:require("../assets/img/works/2.jpg")},
+                {url:require("../assets/img/works/3.jpg")},
+                {url:require("../assets/img/works/4.jpg")},
+                {url:require("../assets/img/works/5.jpg")},
+                {url:require("../assets/img/works/6.jpg")},
+                {url:require("../assets/img/works/7.jpg")},
+                {url:require("../assets/img/works/8.jpg")}
+            ]
 		}
 	}
 }	
 </script>
 <style scoped>
+#inner-headline{
+	background: #14A085;
+    position: relative;
+    margin: 0;
+    padding: 0;
+    color: #fefefe;
+    border-top: 10px solid #11967C;
+}
+.container{
+	padding: 0 20px 0 20px;
+    position: relative;
+    width: 1170px;
+    margin-right: auto;
+    margin-left: auto;
 
+}
+.list-row{
+	margin-bottom: 30px;
+	margin-left: -15px;
+    margin-right: -15px;
+    display: inline-block;
+}
+.col-lg-12 {
+    width: 100%;
+    position: relative;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+.pageTitle {
+    color: #fff;
+    margin: 30px 0 3px;
+}
+.div{
+	display: block;
+}
+h1, h2, h3, h4, h5, h6 {
+    font-family: 'Open Sans', Arial, sans-serif;
+    font-weight: 700;
+    line-height: 1.1em;
+    color: #333;
+    margin-top: 20px;
+}
+h2{
+    font-size: 30px;
+}
+h2 {
+    -webkit-margin-before: 0.83em;
+    -webkit-margin-after: 0.83em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+}
+#inner-content {
+    position: relative;
+    background: #fff;
+    padding: 50px 0 0px 0;
+    width: 100%;
+}
+ul.portfolio-categ {
+    margin: 10px 0 30px 0;
+    padding: 0;
+    float: left;
+    list-style: none;
+}
+ul{
+    display: block;
+    -webkit-margin-before: 1em;
+    -webkit-margin-after: 1em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+    -webkit-padding-start: 40px;
+}
+ul.portfolio-categ li {
+    margin: 0;
+    float: left;
+    list-style: none;
+    font-size: 13px;
+    font-weight: 600;
+    border: 1px solid #D5D5D5;
+    margin-right: 15px;
+}
+li {
+    display: list-item;
+    text-align: -webkit-match-parent;
+}
+ul.portfolio-categ li a {
+    display: block;
+    padding: 8px 20px;
+    color: #14A085;
+    text-decoration: none;
+    background: 0 0;
+}
+a:-webkit-any-link {
+    cursor: auto;
+}
+#thumbs {
+    margin: 0;
+    padding: 0;
+    height: 370px;
+    width:1160px;
+}
+#thumbs li {
+    list-style-type: none;
+}
+#thumbs li {
+    list-style-type: none;
+}
+.item-thumbs {
+    position: relative;
+    overflow: hidden;
+    margin-bottom: 30px;
+    cursor: pointer;
+}
+.col-lg-3 {
+    width: 21%;
+    float: left;
+    min-height: 1px;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+img{
+    max-width: 100%;
+    height: auto;
+}
+.item-thumbs a + img {
+    width: 100%;
+}
+.item-thumbs img {
+    margin-bottom: 15px;
+}
+img {
+    vertical-align: middle;
+}
+img {
+    border: 0;
+}
 </style>
